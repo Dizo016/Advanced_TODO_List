41175da44f10bffbcaa2f1207f6091acd43a83a8
Dizo016 <diegossilva2008@icloud.com>
2026-06-04 12:30:29 -0300

    feat: Conclusão, edição e exclusão configuradas no JavaScript, identação e formatação aplicadas para prevenir bugs

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+let oldInputValue
+
 // Funções
 const saveTodo = (text) => {
 
@@ -24,7 +26,7 @@ const saveTodo = (text) => {
     doneBtn.innerHTML = '<i class="fa-solid fa-check"><i/>'
     //adicionando botão ao elemento
     todo.appendChild(doneBtn)
-    
+
     const editBtn = document.createElement("button")
     editBtn.classList.add("edit-todo")
     editBtn.innerHTML = '<i class="fa-solid fa-pen"><i/>'
@@ -41,6 +43,26 @@ const saveTodo = (text) => {
     todoInput.focus()
 }
 
+//editar
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach(todo => {
+        let todoTitle = todo.querySelector("h3")
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    });
+}
+
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
+
 // Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -51,4 +73,47 @@ todoForm.addEventListener("submit", (e) => {
 
         saveTodo(inputValue)
     }
+})
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div")
+    let todoTitle
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText
+
+    }
+
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done")
+    }
+
+    if (targetEl.classList.contains("remove-todo")) {
+        parentEl.remove()
+    }
+
+    if (targetEl.classList.contains("edit-todo")) {
+        toggleForms()
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if (editInputValue) {
+        updateTodo(editInputValue)
+    }
+    toggleForms()
 })
